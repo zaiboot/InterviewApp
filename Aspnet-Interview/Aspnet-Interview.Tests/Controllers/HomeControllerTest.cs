cd7c52bbabd0a422c458742488546525dc3a19c5
zaiboot <zaiboot@gmail.com>
2017-02-15 17:35:26 -0600

    Adding partial view

--- Aspnet-Interview/Aspnet-Interview.Tests/Controllers/HomeControllerTest.cs
+++ Aspnet-Interview/Aspnet-Interview.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Aspnet_Interview;
 using Aspnet_Interview.Controllers;
 
 namespace Aspnet_Interview.Tests.Controllers
@@ -25,30 +20,6 @@ namespace Aspnet_Interview.Tests.Controllers
             Assert.IsNotNull(result);
         }
 
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
+      
     }
 }
